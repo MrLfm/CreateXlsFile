f01e8bcb686d36c5b8c854ecb370a6e088289c0b
LFM <fuminglei@gmail.com>
2017-03-04 15:19:58 +0800

    优化代码书写方法

--- CreateXLS/ViewController.m
+++ CreateXLS/ViewController.m
@@ -28,21 +28,17 @@
 }
 
 - (void)createXLSFile {
-    
     // 创建存放XLS文件数据的数组
     NSMutableArray  *xlsDataMuArr = [[NSMutableArray alloc] init];
-    
     // 第一行内容
     [xlsDataMuArr addObject:@"Time"];
     [xlsDataMuArr addObject:@"Address"];
     [xlsDataMuArr addObject:@"Person"];
-    [xlsDataMuArr addObject:@"Origin"];
+    [xlsDataMuArr addObject:@"Reason"];
     [xlsDataMuArr addObject:@"Process"];
     [xlsDataMuArr addObject:@"Result"];
-    
     // 100行数据
     for (int i = 0; i < 100; i ++) {
-        
         [xlsDataMuArr addObject:@"2016-12-06 17:18:40"];
         [xlsDataMuArr addObject:@"GuangZhou"];
         [xlsDataMuArr addObject:@"Mr.Liu"];
@@ -50,44 +46,32 @@
         [xlsDataMuArr addObject:@"TaoBao"];
         [xlsDataMuArr addObject:@"Debt"];
     }
-    
-    // 数组转换为字符串，连接符：@"\t"（功能同键盘上的tab键）
+    // 把数组拼接成字符串，连接符是 \t（功能同键盘上的tab键）
     NSString *fileContent = [xlsDataMuArr componentsJoinedByString:@"\t"];
-    
-    // 字符串转换为可变字符串，方便改变其中的字符
-    NSMutableString *muStr = [NSMutableString stringWithString:fileContent];
-    
-    // 新建一个可变数组，存储每行最后一个\t的下标
-    NSMutableArray *rangeMuArr = [[NSMutableArray alloc] init];
-    
+    // 字符串转换为可变字符串，方便改变某些字符
+    NSMutableString *muStr = [fileContent mutableCopy];
+    // 新建一个可变数组，存储每行最后一个\t的下标（以便改为\n）
+    NSMutableArray *subMuArr = [NSMutableArray array];
     for (int i = 0; i < muStr.length; i ++) {
-        
-        NSRange range = [muStr rangeOfString:@"\t" options:NSLiteralSearch range:NSMakeRange(i, 1)];
-        
+        NSRange range = [muStr rangeOfString:@"\t" options:NSBackwardsSearch range:NSMakeRange(i, 1)];
         if (range.length == 1) {
-            
-            [rangeMuArr addObject:@(range.location)];
+            [subMuArr addObject:@(range.location)];
         }
     }
-    
-    // 每行最后一个\t替换为\n
-    for (NSUInteger i = 0; i < rangeMuArr.count; i ++) {
+    // 替换末尾\t
+    for (NSUInteger i = 0; i < subMuArr.count; i ++) {
 #warning  下面的6是列数，根据需求修改
         if ( i > 0 && (i%6 == 0) ) {
-            
-            [muStr replaceCharactersInRange:NSMakeRange([[rangeMuArr objectAtIndex:i-1] integerValue], 1) withString:@"\n"];
+            [muStr replaceCharactersInRange:NSMakeRange([[subMuArr objectAtIndex:i-1] intValue], 1) withString:@"\n"];
         }
     }
-    
     // 文件管理器
     NSFileManager *fileManager = [[NSFileManager alloc]init];
-    
     //使用UTF16才能显示汉字；如果显示为#######是因为格子宽度不够，拉开即可
     NSData *fileData = [muStr dataUsingEncoding:NSUTF16StringEncoding];
-    
     // 文件路径
     NSString *path = NSHomeDirectory();
-    NSString *filePath = [path stringByAppendingPathComponent:@"export.xls"];
+    NSString *filePath = [path stringByAppendingPathComponent:@"/Documents/export.xls"];
     NSLog(@"文件路径：\n%@",filePath);
     // 生成xls文件
     [fileManager createFileAtPath:filePath contents:fileData attributes:nil];
